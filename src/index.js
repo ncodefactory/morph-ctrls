d84aa24701734c39176ed21f0254e82dd7ec3632
Bartosz Solarski <bartosz.solarski@hotmail.com>
2019-04-23 22:25:39 +0200

    to index.js css added and any references for dynamic styling and validate props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import { ContentControl } from './lib';
 
 ReactDOM.render(<ContentControl />, document.getElementById('root'));
